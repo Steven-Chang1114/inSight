a92b2e4c3854587d20c913e1516900d5223e6d7f
Steven Chang <1632460272@qq.com>
2020-02-29 15:54:55 +0000

    Detect the current location

--- app/src/main/java/com/example/greentrip/MainActivity.java
+++ app/src/main/java/com/example/greentrip/MainActivity.java
@@ -1,14 +1,87 @@
 package com.example.greentrip;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    Location currentLocation;
+    FusedLocationProviderClient client;
+    private static final int REQUEST_CODE = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button start = (Button) findViewById(R.id.start);
+        //The placeholder will greet based on your current loaction
+        final TextView greeting = (TextView) findViewById(R.id.greeting);
+        //Find current location
+        client = LocationServices.getFusedLocationProviderClient(this);
+
+        if(ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+            return;
+        }
+        Task<Location> task = client.getLastLocation();
+
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                double longtitude = location.getLongitude();
+                double latitude = location.getLatitude();
+
+                Geocoder geocoder = new Geocoder(MainActivity.this, Locale.getDefault());
+                try {
+                    List<Address> addresses = geocoder.getFromLocation(latitude, longtitude, 1);
+                    String cityName = addresses.get(0).getAddressLine(0);
+
+                    if(location != null){
+                        greeting.setText("Hi You are in " + cityName + "now!" );
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
+
+    //private void requestPermission(){
+      //  ActivityCompat.requestPermissions(this, new String[]{ACCESS_FIND_LOCATION}, 1);
+
+    //}
+
 }
